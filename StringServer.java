02050bfd37cfaa733eff38454d03d2945d2fd6c8
Big-N8 <146897977+Big-N8@users.noreply.github.com>
2023-10-22 10:44:02 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -2,27 +2,27 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     int num = 0; 
     String txt = " "; 
 
-    public String handleRequest(URI url) {
-        if (url.getPath().equals("/add")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
-            return "404 Not Found!";
-        }
+    public String handleRequest(URI url) 
+    {
+        if (url.getPath().equals("/")) 
+        {
+            return String.format("%d.", num);
+        // } else if (url.getPath().equals("/increment")) {
+        //     num += 1;
+        //     return String.format("Number incremented!");
+        // } else {
+        //     // if (url.getPath().contains("/add")) {
+        //     //     String[] parameters = url.getQuery().split("=");
+        //     //     if (parameters[0].equals("s")) {
+        //     //         num += Integer.parseInt(parameters[1]);
+        //     //         return String.format("Number increased by %s! It's now %d", parameters[1], num);
+        //     //     }
+        //     // }
+        //     return "404 Not Found!";
+        // }
     }
 }
 
