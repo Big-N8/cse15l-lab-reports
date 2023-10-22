1e375e50e0d9babb09173d9565f4acef98a70acf
Big-N8 <146897977+Big-N8@users.noreply.github.com>
2023-10-22 10:49:55 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -2,21 +2,20 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    int num = 0; 
-    String txt = " "; 
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
 
-    public String handleRequest(URI url) 
-    {
-        if (url.getPath().equals("/")) 
-        {
-            return String.format("%d.", num);
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
         } else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
         } else {
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("s")) {
+                if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
@@ -26,10 +25,8 @@ class Handler implements URLHandler {
     }
 }
 
-class StringServer 
-{
-    public static void main(String[] args) throws IOException 
-  {
+class NumberServer {
+    public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
